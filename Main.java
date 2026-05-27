c36069424246e7ab1a30d1d612fc3c3e3e7d7e96
krishna-namdev <krishnanamdev0023@gmail.com>
2026-05-27 22:38:07 +0530

    my first Java program

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main{
+    
+
+public static void main( String[] args){
+    String name1 ="krishna";
+    String name2 = "atharv";
+    String name3 = name1 + " "+ name2;
+
+    System.out.println (name3);
+    
+    System.out.println(name1.charAt(0));
+    
+    System.out.println(name3.length());
+    
+    String name4 = name2.replace("atharv" , "suryansh");
+    System.out.println(name4);
+    
+    System.out.println(name1.substring (2,3));
+    }    
+}
